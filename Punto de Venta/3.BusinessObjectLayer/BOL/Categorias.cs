4e0998c5e00d63de67aa55c43c1fb3971df77b82
movazquez <42423878+movazquez@users.noreply.github.com>
2018-08-19 00:36:18 -0500

    Create Categorias.cs

---
+++ Punto de Venta/3.BusinessObjectLayer/BOL/Categorias.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOL
+{
+    public class Categorias : BindingList<Categorias>
+    {
+    }
+}
