f4ca73e26a1c1005d3a54356adf570e1a693471c
movazquez <42423878+movazquez@users.noreply.github.com>
2018-08-19 00:24:14 -0500

    Create MarcaDAL.cs

---
+++ Punto de Venta/3.BusinessObjectLayer/BOL/MarcaDAL.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace BOL
+{
+    class MarcaDAL
+    {
+        //Singleton
+        private static volatile TallaDAL instance = null;
+        private static readonly object padlock = new object();
+        private DataAccess dataAccess = DataAccess.Instance();
+
+        private MarcaDAL() { }
+
+        public static MarcaDAL Instace()
+        {
+            if (instance == null)
+                lock (padlock)
+                    if (instance == null)
+                        instance = new TallaDAL();
+            return instance;
+        }
+
+        public int Add(Marca marca)
+        {
+            try
+            {
+                SqlParameter[] parametros = new SqlParameter[2];
+                parametros[0] = new SqlParameter("@descripcion", marca.descripcion);
+                parametros[1] = new SqlParameter("@activo", marca.activo);
+                string query = "stp_marcas_updateadd";
+                return dataAccess.Execute(query, parametros);
+            }
+            catch (Exception ex)
+            {
+
+                throw new ApplicationException("Error Add\n" + ex.Message);
+            }
+        }
+    }
+}
